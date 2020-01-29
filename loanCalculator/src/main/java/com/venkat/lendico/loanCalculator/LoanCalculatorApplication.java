d4c443e7798fc5b0c90b1661f5aa47771c75f93a
PotentialVenkat <60428942+PotentialVenkat@users.noreply.github.com>
2020-01-29 12:23:06 +0100

    Update LoanCalculatorApplication.java
    Swagger meta data.

--- loanCalculator/src/main/java/com/venkat/lendico/loanCalculator/LoanCalculatorApplication.java
+++ loanCalculator/src/main/java/com/venkat/lendico/loanCalculator/LoanCalculatorApplication.java
@@ -14,5 +14,20 @@ public class LoanCalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoanCalculatorApplication.class, args);
 	}
+	
+	@Bean
+	public Docket swaggerConfig() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				// .paths(PathSelectors.ant("/api/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.venkat")).build().apiInfo(apiDetails());
+
+	}
+
+	private ApiInfo apiDetails() {
+		Contact contact = new Contact("Venkat", "https://github.com/PotentialVenkat/", "Venkata.Yellaturi@gmail.com");
+		return new ApiInfoBuilder().title("Lendico Loan repayment  documentation API").description("Calculator")
+				.termsOfServiceUrl(" ").contact(contact).license("Apache License Version 2.0")
+				.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0").version("1.0.0").build();
+	}
 
 }
